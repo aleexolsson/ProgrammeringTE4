134f6245d7050975dbd3b1350ed111f001853270
Alex Olsson <alex@dumbfucks.se>
2022-08-30 09:28:25 +0200

    Added demonstration and program

---
+++ Demonstration.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace __22te4i220830
+{
+    internal class Demonstration
+    {
+        public void RunHelloWord()
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+        }
+
+        public void RunVariable()
+        {
+            //rektangel
+            double bredd = 20;
+            double höjd = 30;
+            double area;
+            area = bredd * höjd
+            Console.WriteLine("rektangel");
+            Console.WriteLine("bredd: " + bredd);
+            Console.WriteLine("höjd: " + höjd);
+            Console.WriteLine("area: " + area);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace _22te4i220830
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demonstration demo = new Demonstration();
+            //demo.RunHelloWord();
+            demo.RunVariable();
+        }
+    }
+}
