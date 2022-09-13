50c7cc073d74806fadec80bca1e488b1eb05ca54
Alex Olsson <alex@dumbfucks.se>
2022-09-13 10:12:53 +0200

    aaayoooo

---
+++ Unity/Crab MF/Assets/Scripts/Crab.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+
+public class Crab : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private float speed = 4;
+    public Text scoreDisplay;
+    private int score = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // Physics enginge will not rotate Crab
+        rb.freezeRotation = true;
+        // remove gravity
+        rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            // walk
+            float angleRadians = rb.rotation * Mathf.PI / 180f;
+            float xSpeed = Mathf.Cos(angleRadians) * speed;
+            float ySpeed = Mathf.Sin(angleRadians) * speed;
+            rb.velocity = new Vector2(xSpeed, ySpeed);
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                // and turn left
+                rb.rotation += 1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                // and turn right
+                rb.rotation -= 1;
+            }
+        }
+        else
+        {
+            // do not move
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.StartsWith("Worm"))
+        {
+            Destroy(collision.gameObject);
+            score = score + 1;
+            scoreDisplay.text = "Score: " + score;
+        }
+    }
+}
