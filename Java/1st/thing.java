1dbe32ed1384930471d6f9db8b75b333c2a79dfa
Alex Olsson <alexolsson04@gmail.com>
2022-11-29 11:09:24 +0100

    added java things

---
+++ Java/1st/thing.java
@@ -0,0 +1,33 @@
+
+/**
+ * thing does things.
+ *
+ * @aleexolsson
+ * @publicAlpha1
+ */
+public class thing
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class thing
+     */
+    public thing()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
